9275b200813a094d624cc2abedb3ff90e57a9cf5
isinnur <isinngnyy@gmail.com>
2023-05-09 12:05:11 +0300

    add context

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,15 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+//Provider, Consumer - GithubContext.Provider
+
+const GithubProvider = ({ children }) => {
+    return (
+        <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+    );
+};
+
+export { GithubProvider, GithubContext };
--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GithubProvider>
+      <App />
+    </GithubProvider>
   </React.StrictMode>
 );
 
