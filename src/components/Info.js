f14fdde486631d97f499c5bdbf57270867e825bf
isinnur <isinngnyy@gmail.com>
2023-05-09 12:28:35 +0300

    add mock data

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,6 +6,7 @@ import { FiUsers, FiUserPlus } from 'react-icons/fi';
 
 const UserInfo = () => {
   const data = React.useContext(GithubContext);
+  console.log(data);
   return <h2>user info component:{data}</h2>;
 };
 
--- src/context/context.js
+++ src/context/context.js
@@ -11,8 +11,11 @@ const GithubContext = React.createContext();
 //Provider, Consumer - GithubContext.Provider
 
 const GithubProvider = ({ children }) => {
+    const [githubUser, setGithubUser] = useState(mockUser);
+    const [repos, setRepos] = useState(mockRepos);
+    const [followers, setFollowers] = useState(mockFollowers);
     return (
-        <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+        <GithubContext.Provider value={{ githubUser, repos, followers }}>{children}</GithubContext.Provider>
     );
 };
 
