8fec306c66388f5eb5758e90c006a4880644a7bb
isinnur <isinngnyy@gmail.com>
2023-05-10 16:29:59 +0300

    repos setup

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,7 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import { useContext } from 'react';
 const Repos = () => {
+  const { repos } = useContext(GithubContext);
+  console.log(repos);
   return <h2>repos component</h2>;
 };
 
