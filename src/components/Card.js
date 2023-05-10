967b9cafcafa5cf8f4d09a8a2b786355fffafe15
isinnur <isinngnyy@gmail.com>
2023-05-10 16:09:21 +0300

    complete card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,34 @@ import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const { avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username
+  } = githubUser;
+  return <Wrapper>
+    <header>
+      <img src={avatar_url} alt={name} />
+      <div>
+        <h4>{name}</h4>
+        <p>@{twitter_username || 'Işınnur Günay'}</p>
+      </div>
+
+      <a href={html_url}>Follow</a>
+    </header>
+
+    <p className='bio'>{bio}</p>
+    <div className='links'>
+      <p><MdBusiness></MdBusiness> {company} </p>
+      <p><MdLocationOn></MdLocationOn> {location || 'earth'} </p>
+      <a href={`https://${blog}`}><MdLink></MdLink>{blog}</a>
+    </div>
+  </Wrapper>;
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
@@ -26,6 +53,7 @@ const Wrapper = styled.article`
     padding: 0.5rem 1rem 0 1rem;
     letter-spacing: var(--spacing);
     font-size: 1rem;
+
   }
   header {
     display: grid;
@@ -37,6 +65,7 @@ const Wrapper = styled.article`
       width: 75px;
       height: 75px;
       border-radius: 50%;
+      object-fit:cover;
     }
     h4 {
       margin-bottom: 0.25rem;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,11 +5,11 @@ import { GithubContext } from '../context/context';
 const Dashboard = () => {
   return (
     <main>
-      <Navbar></Navbar>
-      <Search />
+      {/* <Navbar></Navbar> */}
+      {/* <Search /> */}
       <Info />
       <User />
-      <Repos />
+      {/* <Repos /> */}
     </main>
   );
 };
