3c01194cecf967cb77d6b7381b6c376307dbb3ae
isinnur <isinngnyy@gmail.com>
2023-05-10 17:30:00 +0300

    add pie chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -27,7 +27,8 @@ const Repos = () => {
 
   return <section className='section'>
     <Wrapper className='section-center'>
-      <ExampleChart data={chartData} />
+      <Pie3D data={chartData} />
+      {/* <ExampleChart data={chartData} /> */}
     </Wrapper>
   </section>
 
