9fb53c578cd99d1e2d730c4720b94a822c5e9c08
isinnur <isinngnyy@gmail.com>
2023-05-17 15:09:08 +0300

    add auth0 db

--- src/index.js
+++ src/index.js
@@ -5,12 +5,27 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
+
+//dev-52r1iukj3fzbwnrn.us.auth0.com
+//FgkKnTXQfKP1u3uWLyTTP9B26Wq74rJwimport { Auth0Provider } from '@auth0/auth0-react';
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.Fragment>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-52r1iukj3fzbwnrn.us.auth0.com"
+      clientId="FgkKnTXQfKP1u3uWLyTTP9B26Wq74rJw"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}>
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.Fragment>
 );
 
